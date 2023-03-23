3d409660bf9f34c61c57e1aeaf1c07d95e759b20
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-03-23 17:19:07 -0500

    Salvando

---
+++ 1-classes-atributos-metodos.php
@@ -0,0 +1,14 @@
+<?php
+//Boa prática: Criar nome da class com primeira letra maiúscula
+class Pessoa
+{
+  //Atributos
+  public $nome;
+  public $idade;
+
+  //Metodos
+  public function Falar()
+  {
+    echo "Falou!";
+  }
+}
